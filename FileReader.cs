1dcbf5e8b8c1005d5642ed1c3c1ab6ecfc3ac65f
Stormy-617 <113555439+Stormy-617@users.noreply.github.com>
2025-11-13 08:32:09 -0600

    Add files via upload

---
+++ FileReader.cs
@@ -0,0 +1,53 @@
+﻿public class FileReader
+{
+
+    private static int GetLineCount(string path)
+    {
+        if (!File.Exists(path))
+        {
+            throw new FileNotFoundException($"The file wasn't found!");
+        }
+
+        int lineCount = 0;
+        using StreamReader reader = new StreamReader(path);
+
+        while (!reader.EndOfStream)
+        {
+            reader.ReadLine();
+            lineCount++;
+        }
+        return lineCount;
+    }
+
+    private static List<string> ReadCNoteSharpDataBase(string path)
+    {
+        if (!File.Exists(path))
+        {
+            throw new FileNotFoundException($"The file wasn't found!");
+        }
+        
+        int lineCount = GetLineCount(path);
+        List<string> lines = new List<string>(lineCount - 1);
+        
+        using StreamReader reader = new StreamReader("CNoteSharpDatabase.csv");
+        reader.ReadLine();
+
+        for (int i = 0; i < lineCount - 1; i++)
+        {
+            string line = reader.ReadLine();
+            lines.Add(line);
+        }
+        return lines;
+    }
+
+    private static void WriteCNoteSharpDataBase()
+    {
+        using StreamWriter streamWriter = new StreamWriter("CNoteSharpDatabase.csv");
+    }
+    
+    public static void Main()
+    {
+        string path = "CNoteSharpDatabase.csv";
+        Console.WriteLine(string.Join(" , \n",  ReadCNoteSharpDataBase(path)));
+    }
+}
