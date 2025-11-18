12ac52a48ccd7ce1e9cfc4ec73014c46e4e683ed
BluCherry <cgroves317@gmail.com>
2025-11-17 18:11:16 -0600

    Update FileReader.cs

--- FileReader.cs
+++ FileReader.cs
@@ -38,10 +38,48 @@ public class FileReader
         using StreamReader reader = new StreamReader(path); 
         reader.ReadLine();
 
-        for (int i = 0; i < lineCount; i++)
+        for (int i = 0; i < lineCount - 1; i++)
         {
-            string data =  reader.ReadLine();
-            database.Add(data);
+            string line = reader.ReadLine();
+
+            string[] cols = line.Split(',');
+
+            string type = cols[0];
+
+            if (type == "Track")
+            {
+                string title = cols[1];
+                string creator = cols[3];
+                string album = cols[4];
+                int year = int.Parse(cols[5]);
+                double duration = double.Parse(cols[8]);
+                double rating = double.Parse(cols[9]);
+
+                database.Add(new Track(title, creator, album, year, duration, rating));
+            }
+            if (type == "Audiobook")
+            {
+                string title = cols[1];
+                string creator = cols[3];
+                int year = int.Parse(cols[5]);
+                double duration = double.Parse(cols[8]);
+                string rating = cols[9];
+        
+                database.Add(new Audio_Book(title, creator, year, duration, rating));
+            }
+            if (type == "TV Episodes")
+            {
+                string title = cols[1];
+                string showTitle = cols[2];
+                string creator = cols[3];
+                int year = int.Parse(cols[5]);
+                int seasonNumber = int.Parse(cols[6]);
+                int episodeNumber = int.Parse(cols[7]);
+                double duration = double.Parse(cols[8]);
+                int rating = int.Parse(cols[9]);
+        
+                database.Add(new TV_Episode(title, showTitle, creator, year, seasonNumber, episodeNumber, duration, rating));
+            }
         }
         
         return database;
