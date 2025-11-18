114ba106cb8aa2d23d20b29f96da2b31fce48229
BluCherry <cgroves317@gmail.com>
2025-11-18 15:28:27 -0600

    Update Audio Book.cs
    change rating from string to boolean

--- media classes/Audio Book.cs
+++ media classes/Audio Book.cs
@@ -6,14 +6,12 @@ using System.Threading.Tasks;
 
 namespace Intermediate_CSharp_Final
 {
-    public class Audio_Book : Media<string>
+    public class Audio_Book : Media<bool>
     {
-        public Audio_Book(string title, string creator, int year, double duration, string rating) : base(title, creator, year, duration, rating)
+        public Audio_Book(string title, string creator, int year, double duration, bool rating) : base(title, creator, year, duration, rating)
         {
-            if (!rating.Equals("Thumbs Up") || !rating.Equals("Thumbs Down"))
-            {
-                throw new ArgumentOutOfRangeException("rating for Audiobook must be Thumbs up or Thumbs down");
-            }
+            
         }
     }
 }
+
